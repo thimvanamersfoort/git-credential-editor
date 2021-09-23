049076f3ab92a39baab08b71c8f7a34b6c7164b5
thimvanamersfoort <thimvanamersfoort@gmail.com>
2021-09-23 22:08:53 +0200

    Added prompt function to app.js

--- app.js
+++ app.js
@@ -7,7 +7,32 @@ const git_shell = require('./controllers/git_shell');
 
 let git = new git_shell();
 
+fs.readFile('./access.json', 'utf-8', (err, contents) => {
+  if(err) throw new Error(err.message);
 
+  // get user data and map to new array to use in questions
+  var data = JSON.parse(contents);
+  var questions = data.map((orig) => {
+    return {
+      title: orig.username,
+      value: orig.username
+    }
+  });
 
+  // Get username to change
+  var response;
+  (async () => {
+    response = await prompts({
+      type: 'select',
+      name: 'value',
+      message: 'Pick a username',
+      choices: questions,
+      initial: 1
+    });
 
+    console.log(response);
+  })();
+  
+
+})
 
